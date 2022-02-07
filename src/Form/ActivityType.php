fd44f6e70ac5a4cb79c1c9a67db37c9cdad9e092
JULIENSAVEAN <j.savean@gmail.com>
2022-02-07 12:26:37 +0100

    new page

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -6,17 +6,31 @@ use App\Entity\Activity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ActivityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
             ->add('description')
-            ->add('date')
-            ->add('picture')
-            ->add('capacity')
+            ->add('date', DateType::class, [
+                'widget' => 'choice',
+                'input'  => 'datetime_immutable',
+                'html5' => false,
+                // this is actually the default format for single_text
+                'format' => 'dd-MMMM-yyyy',
+            ])
+            ->add('picture', TextType::class, [
+                'label' => 'Image',
+            ])
+            ->add('capacity', TextType::class, [
+                'label' => 'Capacité',
+            ])
             ->add('sport', null, ['choice_label' => 'name'])
         ;
     }
